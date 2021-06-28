ee89efb041e6b57acd8fcde61a5d82ffd17d944c
Khoushick S <86069674+Khoushick-S@users.noreply.github.com>
2021-06-28 16:59:36 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Info from "./info";
+
+function App(){
+  return(
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+      <Info />
+      
+    </div>
+  );
+}
+
+
+export default App;
---
+++ info.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Info(){
+  return(
+    <div className="note">
+      <h1>Javascript : </h1>
+      <p>Javascript JavaScript is high-level, often just-in-time compiled, and multi-paradigm. It has curly-bracket syntax, dynamic typing, prototype-based object-orientation, and first-class functions</p>
+    </div>
+
+  );
+}
+
+export default Info;
